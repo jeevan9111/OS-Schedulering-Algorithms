e588620065be101da87014818e9a4b3ba7f379f8
Jeevan Thapa <jeevanthapa9111@gmail.com>
2018-07-08 21:26:56 +0545

    changed the lower portion to empty the queue fully

--- FCFS.py
+++ FCFS.py
@@ -1,23 +1,22 @@
 import JobSpecification
-from Queue import Queue
+from queue import Queue
 
 jobList = JobSpecification.jobs
 jobs = jobList
 jobQueue = Queue(maxsize=0)
 time = 0
-
+print("PID \t ArrivalTime \tStarted Time \t Compelted Time")
 while len(jobs) > 0:
 
     for job in jobs:
         if time < job['ArrivalTime']:
             break
-        jobQueue.put (job['BurstTime'])
+        jobQueue.put(job)
 
         jobs = [j for j in jobs if j != job]
 
-    while True:
-        BurstTime = jobQueue.get()
-        if BurstTime > 0:
-            time = time + BurstTime
-            print(BurstTime)
-        break
+    while not jobQueue.empty():
+        PID, ArrivalTime, BurstTime = jobQueue.get()
+        print(PID, "\t", ArrivalTime, "\t\t", time, end="\t\t\t")
+        time = time + BurstTime
+        print(time)
